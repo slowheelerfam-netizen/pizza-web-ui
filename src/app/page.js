a7d55da94d7dbbb60a966d974ffa703f0f02d986
donwheeler <slowheelerfam@gmail.com>
2026-02-04 14:56:55 -0800

    trying to solve

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,8 @@
 export default function Home() {
   return (
-    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Internal Pizza System</h1>
 
-      <p>This is an internal application.</p>
+      'use client'
 
-      <ul>
-        <li><a href="/register">Register</a></li>
-        <li><a href="/kitchen">Kitchen</a></li>
-        <li><a href="/monitor">Monitor</a></li>
-        <li><a href="/oven">Oven Station</a></li>
-      </ul>
-    </main>
-  )
+export default function RegisterPage() {
+  return <div>Register page placeholder</div>
 }
