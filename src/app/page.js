ed31a275f9f64d6b02e907186d69d9bc3410fa53
donwheeler <slowheelerfam@gmail.com>
2026-02-04 14:50:17 -0800

    fixed home page, adding instruction how to use site

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Public website coming soon.</h1>
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Internal Pizza System</h1>
+
+      <p>This is an internal application.</p>
+
+      <ul>
+        <li><a href="/register">Register</a></li>
+        <li><a href="/kitchen">Kitchen</a></li>
+        <li><a href="/monitor">Monitor</a></li>
+        <li><a href="/oven">Oven Station</a></li>
+      </ul>
     </main>
   )
 }
