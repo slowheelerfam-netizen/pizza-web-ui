da45b0c6c82e176bb4ab295d35c66a0c4940c343
donwheeler <slowheelerfam@gmail.com>
2026-02-19 18:08:13 -0800

    Final Push

--- src/lib/appConfig.js
+++
@@ -1,3 +0,0 @@
-export const DEMO_MODE =
-  process.env.NEXT_PUBLIC_DEMO_MODE === 'true' ||
-  process.env.DEMO_MODE === 'true'
--- src/lib/isDemoMode.js
+++
@@ -1,5 +0,0 @@
-import { APP_CONFIG } from './appConfig'
-
-export function isDemoMode() {
-  return APP_CONFIG.DEMO_MODE === true
-}
