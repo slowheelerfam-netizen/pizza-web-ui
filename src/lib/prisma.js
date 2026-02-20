90a74872feba3ce57cd6aa2f5cb43d655ebb3374
donwheeler <slowheelerfam@gmail.com>
2026-02-20 09:50:27 -0800

    fixed issues with Prisma.  UI structure Good and solid

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,25 +1,13 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg'
-import 'dotenv/config'
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
-const globalForPrisma = global
+const globalForPrisma = global;
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL })
-const adapter = new PrismaPg(pool)
-
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-    log:
-      process.env.NODE_ENV === 'development'
-        ? ['query', 'error', 'warn']
-        : ['error'],
-  })
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
+if (!globalForPrisma.prisma) {
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaPg(pool);
+  globalForPrisma.prisma = new PrismaClient({ adapter });
 }
 
-export { prisma }
+export const prisma = globalForPrisma.prisma;
