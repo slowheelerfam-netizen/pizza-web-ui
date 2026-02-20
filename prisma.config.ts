f443cc98537c1d9fda685a749485e0ea52db269f
donwheeler <slowheelerfam@gmail.com>
2026-02-20 09:59:19 -0800

    Fixed prisma.config.ts, corrected vercel build error> Should be most current STABLE UI RESTORE POINT

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,15 +1,5 @@
 import { defineConfig } from 'prisma/config';
-import 'dotenv/config';
 
 export default defineConfig({
-  earlyAccess: true,
   schema: 'prisma/schema.prisma',
-  migrate: {
-    async adapter() {
-      const { Pool } = await import('pg');
-      const { PrismaPg } = await import('@prisma/adapter-pg');
-      const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-      return new PrismaPg(pool);
-    },
-  },
 });
